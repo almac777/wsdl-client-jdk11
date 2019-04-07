7e094ff12ec7c63fe0d02ac51d35d5cab548c7b8
Alex M. Macovei <alex.markus.macovei@gmail.com>
2019-04-07 17:11:34 +0200

    Some more refactoring

--- src/main/java/at/fhcampus/se/wsdlclient/WsdlClientApplication.java
+++ src/main/java/at/fhcampus/se/wsdlclient/WsdlClientApplication.java
@@ -1,6 +1,7 @@
 package at.fhcampus.se.wsdlclient;
 
 import at.fhcampus.se.wsdlclient.calculator.AddResponse;
+import at.fhcampus.se.wsdlclient.calculator.SubtractResponse;
 import at.fhcampus.se.wsdlclient.soapclients.CalculatorClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,17 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class WsdlclientApplication {
+public class WsdlClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WsdlclientApplication.class, args);
+        SpringApplication.run(WsdlClientApplication.class, args);
     }
 
     @Bean
     CommandLineRunner lookup(CalculatorClient calculatorClient) {
         return args -> {
-            AddResponse response = calculatorClient.add(10, 20);
-            System.out.println(">> " + response.getOut());
+            AddResponse response1 = calculatorClient.add(10, 20);
+            SubtractResponse response2 = calculatorClient.subtract(10, 20);
+            System.out.println(">> 10 + 20 " + response1.getOut());
+            System.out.println(">> 10 - 20 " + response2.getOut());
         };
     }
 
